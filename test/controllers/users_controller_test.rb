64d847733463f1005c2fef19bcd93e03215462eb
masaya-ishioka <penpenmono31@gmail.com>
2019-01-01 15:16:46 +0000

    1/1finish

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class SoundControllerTest < ActionDispatch::IntegrationTest
+class UsersControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
