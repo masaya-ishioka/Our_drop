1cb9fb03261174c70bde3f247439e605f57b41ff
masaya-ishioka <penpenmono31@gmail.com>
2019-01-06 06:14:54 +0000

    finish_likes

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,11 +3,31 @@ class CommentsController < ApplicationController
 	end
 
 	def update
+		@comment = Comment.find_by(id: params[:id])
+		if @comment.update(comment: params[:comment])
+			redirect_to sounds_path(params[:sound_id])
+			flash[:success] = "コメントを更新しました。"
+		else
+			render sounds_path(params[:sound_id])
+			flash[:danger] = "更新に失敗しました。"
+		end
 	end
 
 	def create
+		@comment = Comment.new(user_id: @current_user.id, sound_id: params[:sound_id], comment: params[:comment])
+		if @comment.save
+			redirect_to sounds_path(params[:sound_id])
+			flash[:success] = "コメントを投稿しました。"
+		else
+			render sounds_path(params[:sound_id])
+			flash[:danger] = "投稿に失敗しました。"
+		end
 	end
 
 	def destroy
+		@comment = Comment.find_by(id: params[:id])
+		@comment.destroy
+		redirect_to sounds_path(params[:sound_id])
+		flash[:danger] = "コメントを削除しました。"
 	end
 end
--- app/models/like_user.rb
+++ app/models/like_user.rb
@@ -2,5 +2,5 @@ class LikeUser < ApplicationRecord
 	validates :user_id, presence: true
 	validates :my_id, presence: true
 
-	belongs_to :user, dependent: :destroy
+	belongs_to :user
 end
