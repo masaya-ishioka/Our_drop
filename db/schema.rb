557d5defa3b0b55e0718c75c41289849c971977a
masaya-ishioka <penpenmono31@gmail.com>
2019-01-08 11:23:32 +0000

    fix_some

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_05_103137) do
+ActiveRecord::Schema.define(version: 2019_01_08_074146) do
 
   create_table "comments", force: :cascade do |t|
     t.text "comment"
@@ -59,6 +59,9 @@ ActiveRecord::Schema.define(version: 2019_01_05_103137) do
     t.string "twitter"
     t.string "instagram"
     t.string "youtube"
+    t.string "activation_digest"
+    t.boolean "activated", default: false
+    t.datetime "activated_at"
   end
 
 end
