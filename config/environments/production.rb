f1ee32399d4c3d4ceeea687f305ac583e3efe80a
masaya,ishioka <44196318+masaya-ishioka@users.noreply.github.com>
2019-01-12 20:32:56 +0900

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,7 +71,7 @@ Rails.application.configure do
       port: 587,
       address: 'smtp.gmail.com',
       domain: 'smtp.gmail.com',
-  
+ 
       enable_starttls_auto: true
   }
   host = 'https://our-drop.herokuapp.com'                     # ローカル環境
